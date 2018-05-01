6b262c1167ddc7d326de96423998bda33d3fadc2
viicky <victor.creusel@gmail.com>
2018-05-01 16:24:55 +0200

    Ajout des fichiers de bases d'apprentissage et de test

---
+++ src/LectureMessage.java
@@ -0,0 +1,23 @@
+import java.io.File;
+
+/**
+ * Lire un message
+ * @author Victor
+ *
+ */
+public class LectureMessage {
+
+	public LectureMessage() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	/**
+	 * 
+	 * @param f fichier dans lequel lire le message
+	 * @return Tableau contenant les mots du message (sans les mots de 3 lettres) en minuscules
+	 */
+	public String[] lireMessage(File f) {
+		
+	}
+}
