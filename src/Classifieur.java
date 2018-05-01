933387ba3d092e030c1491f6d6082eaa9414f033
viicky <victor.creusel@gmail.com>
2018-05-01 17:54:44 +0200

    Squellette de l'apprentissage

---
+++ src/Classifieur.java
@@ -0,0 +1,17 @@
+
+public class Classifieur {
+	
+	private double probaSpam;
+	private double[] probaMotSpam;
+
+	public Classifieur() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	
+	public void apprendre(String[] dico, int nombreHam, int nombreSpam) {
+		
+	}
+
+}
