4b05344576565cf700264d907c83af77a0e03cec
viicky <victor.creusel@gmail.com>
2018-05-02 18:28:54 +0200

    Commentaire de Probabilite

--- src/Probabilite.java
+++ src/Probabilite.java
@@ -1,6 +1,14 @@
 
+/**
+ * Classe utilitaire permettant de sauvegarder les probabilités qu'un message soit un spam ou un ham
+ * @author Victor
+ *
+ */
 public class Probabilite {
 	
+	/**
+	 * Se comporte comme un structure en C
+	 */
 	public double spam;
 	public double ham;
 
