5fa4b8635f47551b8cf5e826b540550f7d62f4fd
Mohammade <dalati.m.t@hotmail.fr>
2018-05-02 17:59:23 +0200

    Modif filtreAntiSpam

--- src/ApprendFiltre.java
+++ src/ApprendFiltre.java
@@ -3,7 +3,7 @@ public class ApprendFiltre {
 	public ApprendFiltre(String classifieur, String baseapp, int nombreSpam, int nombreHam) {
 		String[] dico = ChargerDictionnaire.chargerDictionnaire("res/dictionnaire1000en.txt");
 		Classifieur c = new Classifieur(dico);
-		System.out.println("Apprentissage sur " + nombreSpam + "spams et " + nombreHam + "hams ...");
+		System.out.println("Apprentissage sur " + nombreSpam + " spams et " + nombreHam + " hams ...");
 		c.apprendre(baseapp, nombreSpam, nombreHam);
 		c.save(classifieur);
 		System.out.println("Classifieur enregistré dans '" + classifieur + "'.");
--- src/FiltreAntiSpam.java
+++ src/FiltreAntiSpam.java
@@ -62,8 +62,54 @@ public class FiltreAntiSpam {
 		sc.close();
 	}
 	
+	public FiltreAntiSpam(String c, String dico, String basetest, int nombreSpam, int nombreHam) {
+		// Chargement du dictionnaire
+		String[] dictionnaire = ChargerDictionnaire.chargerDictionnaire(dico);
+
+		Classifieur classifieur = Classifieur.load(c);
+		
+		// Classification
+		System.out.println("Test :");
+		int nbErreursSpam = 0, nbErreursHam = 0;
+		for (int i = 0; i < nombreSpam; i++) {
+			String[] message = LectureMessage.lireMessage(new File(basetest + "/spam/" + i + ".txt"));
+			boolean[] b = LectureMessage.comparaisonDico(dictionnaire, message);
+			
+			//Probabilite proba = classifieur.probaSpam(b);
+			boolean sp = classifieur.classifierSpam(b);
+			System.out.print("SPAM n°" + i + " détecté comme " + (sp ? "SPAM" : "HAM"));
+			if(sp) {
+				System.out.println(" : OK");
+			}else {
+				System.out.println(" : **ERREUR**");
+				nbErreursSpam++;
+			}
+			//System.out.println("P(Y=SPAM | X=x) = " + proba.spam + " ; P(Y=HAM | X=x) = " + proba.ham);
+		}
+		for (int i = 0; i < nombreHam; i++) {
+			String[] message = LectureMessage.lireMessage(new File(basetest + "/ham/" + i + ".txt"));
+			boolean[] b = LectureMessage.comparaisonDico(dictionnaire, message);
+			
+			//Probabilite proba = classifieur.probaSpam(b);
+			boolean sp = classifieur.classifierSpam(b);
+			System.out.print("HAM n°" + i + " détecté comme " + (sp ? "SPAM" : "HAM"));
+			if(sp) {
+				System.out.println(" : **ERREUR**");
+				nbErreursHam++;
+			}else {
+				System.out.println(" : OK");
+			}
+			//System.out.println("P(Y=SPAM | X=x) = " + proba.spam + " ; P(Y=HAM | X=x) = " + proba.ham);
+		}
+		
+		System.out.println("=====================================================================================");	
+		System.out.println("Erreur de test sur les " + nombreSpam + " SPAM : " + ((double)nbErreursSpam) / ((double)nombreSpam) * 100 + " %");
+		System.out.println("Erreur de test sur les " + nombreHam + " HAM : " + ((double)nbErreursHam) / ((double)nombreHam) * 100 + " %");
+		System.out.println("Erreur de test globale sur " + (nombreSpam + nombreHam) + " mails : " + ((double)(nbErreursSpam + nbErreursHam)) / ((double)(nombreSpam + nombreHam)) * 100 + " %");
+	}
+	
 	public static void main(String[] args) {
-		new FiltreAntiSpam("res/dictionnaire1000en.txt", args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+		new FiltreAntiSpam(args[0], "res/dictionnaire1000en.txt", args[1], Integer.parseInt(args[2]), Integer.parseInt(args[3]));
 	}
 
 }
